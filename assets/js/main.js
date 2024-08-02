f35ec6147263ee63ce37e8e77512b365b3e09678
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-08-02 19:32:20 +0300

    save

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,73 @@
-window.addEventListener("scroll", function () {
-  document
-    .getElementById("header-nav")
-    .classList.toggle("headernav-scroll", window.scrollY > 135);
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})();
+
+window.addEventListener('scroll', function () {
+    document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
 });
 
-const offcanvasCartEl = document.getElementById("offcanvasCart");
+const offcanvasCartEl = document.getElementById('offcanvasCart');
 const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
 
-document.getElementById("cart-open").addEventListener("click", (e) => {
-  e.preventDefault();
-  offcanvasCart.toggle();
+document.getElementById('cart-open').addEventListener('click', (e) => {
+    e.preventDefault();
+    offcanvasCart.toggle();
 });
 
-document.querySelectorAll(".closecart").forEach((item) => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    offcanvasCart.hide();
-    let href = item.dataset.href;
-    document.getElementById(href).scrollIntoView();
-  });
+document.querySelectorAll('.closecart').forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        offcanvasCart.hide();
+        let href = item.dataset.href;
+        document.getElementById(href).scrollIntoView();
+    });
 });
+
 
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $("#top").fadeIn();
-    } else {
-      $("#top").fadeOut();
-    }
-  });
-
-  $("#top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 500);
-    return false;
-  });
-
-  $(".owl-carousel-full").owlCarousel({
-    margin: 20,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      500: {
-        items: 2,
-      },
-      700: {
-        items: 3,
-      },
-      1000: {
-        items: 4,
-      },
-    },
-  });
-});
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
         }
+    });
 
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+    $('#top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+    $(".owl-carousel-full").owlCarousel({
+        margin: 20,
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 2
+            },
+            700: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
